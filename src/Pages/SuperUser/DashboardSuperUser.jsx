077d44ce858420e81bad2f4146557f23ee08cac3
Davila0911 <davidalejandroavilacastro@gmail.com>
2026-01-11 20:36:43 -0600

    SuperU logoutBtn

--- src/Pages/SuperUser/DashboardSuperUser.jsx
+++ src/Pages/SuperUser/DashboardSuperUser.jsx
@@ -1,11 +1,22 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function SuperUserDashboard() {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("token"); // borrar JWT
+    navigate("/login"); // redirigir al login
+  };
+
   return (
     <div>
       <h1>Panel SuperAdmin</h1>
       <Link to="/superuser/create">Crear Usuario</Link><br/>
-      <Link to="/superuser/edit">Editar Usuario</Link>
+      <Link to="/superuser/edit">Editar Usuario</Link><br/><br/>
+
+      <button onClick={handleLogout} style={{ padding: "8px 16px", cursor: "pointer" }}>
+        Cerrar Sesión
+      </button>
     </div>
   );
 }
